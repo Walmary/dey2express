abc5bfd325c50a83b6a89604d6b6a4f8aeffc51a
Walter - Munoz Oliver <38053862+Walmary@users.noreply.github.com>
2022-05-18 04:02:36 -0400

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const fruits = [{
+        name: 'apple',
+        color: 'red',
+        readyToEat: true
+    },
+    {
+        name: 'pear',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name: 'banana',
+        color: 'yellow',
+        readyToEat: true
+    }
+];
+
+app.get('/fruits/:indexOfFruitsArray', (req, res) => {
+    res.send(fruits[req.params.indexOfFruitsArray]);
+});
